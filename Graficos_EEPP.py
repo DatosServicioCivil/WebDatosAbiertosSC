c332da12f436bf74a731fdf85321ccdc6647d482
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-08-28 15:25:36 -0400

    Update Graficos_EEPP.py
    titulo eje y grafico 1: none

--- Graficos_EEPP.py
+++ Graficos_EEPP.py
@@ -22,7 +22,7 @@ visible_y_axis=True
 #----------------------------------------------------------------------------------------------------------------------------
 # grafico Evolución de Postulaciones por Año
 graf1=px.line(df_postulaciones,x='año',y='postulaciones',title='<b>Evolución de Postulaciones por Año</b>').\
-        update_yaxes(visible=visible_y_axis).\
+        update_yaxes(visible=visible_y_axis,title_text=None).\
                 update_xaxes(title_text=None)
 
 #----------------------------------------------------------------------------------------------------------------------------
