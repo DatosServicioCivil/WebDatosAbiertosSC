1c9d380cff3cb6a39075094ff9f02a02a308bed6
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-08-28 14:38:03 -0400

    Update Graficos_EEPP.py line_shape

--- Graficos_EEPP.py
+++ Graficos_EEPP.py
@@ -39,9 +39,9 @@ graf2 = px.line(
 
 # Add lines for "Mujeres" and "Hombres"
 graf2.add_trace(
-    go.Scatter(x=df_mujeres['year'], y=df_mujeres['Porcentaje'], mode='lines', name='Mujeres')
+    go.Scatter(x=df_mujeres['year'], y=df_mujeres['Porcentaje'], mode='lines',line_shape='spline', name='Mujeres')
 )
-graf2.add_trace(go.Scatter(x=df_hombres['year'], y=df_hombres['Porcentaje'], mode='lines', name='Hombres'))
+graf2.add_trace(go.Scatter(x=df_hombres['year'], y=df_hombres['Porcentaje'], mode='lines',line_shape='spline', name='Hombres'))
 
 #----------------------------------------------------------------------------------------------------------------------------
 # grafico Postulación Promedio por Año
