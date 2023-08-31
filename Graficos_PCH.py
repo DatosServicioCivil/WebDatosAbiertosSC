c525d093d4e3c16eec313c205f18b171db0b447a
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-08-31 08:40:34 -0400

    Update Graficos_PCH cambios en formato de texto

--- Graficos_PCH.py
+++ Graficos_PCH.py
@@ -53,4 +53,4 @@ col3, col4=st.columns(2,gap='small')
 with col3:
         st.plotly_chart(graf3,use_container_width=True)
 with col4:
-        st.text('Prácticas Chile es un programa gestionado por el Servicio Civil, \nque busca promover y atraer talento joven al Estado, y que permite a estudiantes de carreras \nuniversitarias y técnicas realizar sus prácticas en ministerios y servicios públicos, poniendo al servicio del país sus conocimientos y habilidades.')
+        st.text('Prácticas Chile es un programa gestionado por el Servicio Civil, \nque busca promover y atraer talento joven al Estado, y que permite a \nestudiantes de carreras universitarias y técnicas realizar sus prácticas en ministerios \ny servicios públicos, poniendo al servicio del país sus conocimientos y habilidades.')
