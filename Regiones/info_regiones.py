637812094d6af896b2f109aabc53bf74284bccf3
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-09-26 15:02:23 -0300

    Update info_regiones.py

--- Regiones/info_regiones.py
+++ Regiones/info_regiones.py
@@ -4,3 +4,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 st.set_page_config(layout='wide')
+
+st.title('Información Regional', anchor=None, *, help=None)
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.markdown("*Conoce la información del Servicio Civil por regiones*.")
+
+with col2:
+   st.header("Principales Indicadores")
+
+with col3:
+   st.image("imagenes/Fotos_Regiones/biobio.jpg")
