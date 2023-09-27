487a2820ffcbccb14069c479d76ed1ff793ebc07
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-09-27 16:07:57 -0300

    Update info_regiones.py

--- info_regiones.py
+++ info_regiones.py
@@ -13,7 +13,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
    st.markdown("**Conoce la información del Servicio Civil por regiones**.")
    st.markdown("Selecciona zona y región")
-   col4, col5, col6, col7=st.columns(4)
+   col4, col5, col6=st.columns(3)
    with col4:
       zona=st.selectbox("Zona", ["Norte", "Centro", "Sur", "Austral"])
    with col5:
