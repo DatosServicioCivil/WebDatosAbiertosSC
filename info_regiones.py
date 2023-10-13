b5a7aa652ab430552b1042a76a59f62288ed26f8
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-10-13 12:10:54 -0300

    Update info_regiones.py

--- info_regiones.py
+++ info_regiones.py
@@ -44,7 +44,7 @@ with col2:
     </tr>
   </tbody>
 </table>
-<p><td style="text-align: center; font-family: Arial; font-size: 10px;"><span style="color: #999999;">Fecha Actualizacion: 30/09/2023</span></td>.</p>
+<p><td style="text-align: center; font-family: Arial; font-size: 8px;"><span style="color: #999999;">Fecha Actualizacion: 30/09/2023</span></td>.</p>
    """
    components.html(html_table)
       
