0cb7d31b7de3de5db868b139c567cdf45bc4bcb1
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-10-13 12:33:19 -0300

    Update info_regiones.py

--- info_regiones.py
+++ info_regiones.py
@@ -13,7 +13,7 @@ st.title('Información Regional', anchor=None, help=None)
 
 col1, col2, col3 = st.columns(spec=[0.2,0.6,0.2],gap='small')
 
-df_kpi_region=pd.read_csv('kpi/indicadores_regionales.csv',sep=";")
+df_kpi_region=pd.read_csv('kpi/indicadores_regionales.csv',sep=";",encoding='utf-8')
 kpi1=df_kpi_regional[df_kpi_regional.id=='t_1_1'].resultado.value[0]
 # df_postulaciones=pd.read_csv('EEPP/postulaciones_x_año.csv',encoding='utf-8') 
 
