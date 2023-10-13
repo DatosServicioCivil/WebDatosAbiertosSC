c27fc8a3c181a7365b80612b3a8892f41389334e
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-10-13 15:17:41 -0300

    Create home.py

---
+++ home.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+# import snowflake.connector
+import pandas as pd
+from PIL import Image
+import plotly.express as px
+import numpy as np
+# import base64
+import time
+# import altair as alt
+import streamlit.components.v1 as components
+
+
+st.sidebar.header("Configuration")
+
+# This function sets the logo and company name inside the sidebar
+def add_logo(logo_path, width, height):
+    """Read and return a resized logo"""
+    logo = Image.open(logo_path)
+    modified_logo = logo.resize((width, height))
+    return modified_logo
+
+my_logo = add_logo(logo_path="./images/DeltaPacific_Logo.jpg", width=200, height=100)
+st.sidebar.image(my_logo)
+st.sidebar.subheader("Delta Pacific Beverage Co.")
+
+# Set Page Header
+st.header("Delta Pacific Beverage Chain Dashboard")
+# Set custom CSS for hr element
+st.markdown(
+    """
+        <style>
+            hr {
+                margin-top: 0.0rem;
+                margin-bottom: 0.5rem;
+                height: 3px;
+                background-color: #333;
+                border: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Add horizontal line
+st.markdown("<hr>", unsafe_allow_html=True)
