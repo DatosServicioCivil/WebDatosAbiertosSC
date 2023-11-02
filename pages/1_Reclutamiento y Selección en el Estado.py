9b3745e9f0ff3e8609531b21f29cff06e82c08d2
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-02 17:30:45 -0300

    Update 1_Reclutamiento y Selección en el Estado.py

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -98,17 +98,17 @@ if a=='Alta Dirección Pública':
            option_3 = st.selectbox('Ministerio',Ministerios)
 
     if option_1=='Todos' and option_2=='Todos' and option_3=='Todos':
-        publicaciones=df_concursos_adp.groupby('Year_Convocatoria').agg({'CD_Concurso':'count'}).reset_index()
+        publicaciones=df_concursos_adp.groupby('Year_Convocatoria').agg({'Concursos':'sum'}).reset_index()
     if option_1!='Todos' and option_2=='Todos' and option_3=='Todos':
-        publicaciones=df_concursos_adp[df_concursos_adp.Nivel==option_1].groupby('Year_Convocatoria').agg({'CD_Concurso':'count'}).reset_index()
+        publicaciones=df_concursos_adp[df_concursos_adp.Nivel==option_1].groupby('Year_Convocatoria').agg({'Concursos':'sum'}).reset_index()
     if option_1!='Todos' and option_2!='Todos' and option_3=='Todos':
-        publicaciones=df_concursos_adp[(df_concursos_adp.Nivel==option_1) & (df_concursos_adp.Region==option_2)].groupby('Year_Convocatoria').agg({'CD_Concurso':'count'}).reset_index()
+        publicaciones=df_concursos_adp[(df_concursos_adp.Nivel==option_1) & (df_concursos_adp.Region==option_2)].groupby('Year_Convocatoria').agg({'Concursos':'sum'}).reset_index()
     if option_1!='Todos' and option_2!='Todos' and option_3!='Todos':
-        publicaciones=df_concursos_adp[(df_concursos_adp.Nivel==option_1) & (df_concursos_adp.Region==option_2) & (df_concursos_adp.Ministerio==option_3)].groupby('Year_Convocatoria').agg({'CD_Concurso':'count'}).reset_index()
+        publicaciones=df_concursos_adp[(df_concursos_adp.Nivel==option_1) & (df_concursos_adp.Region==option_2) & (df_concursos_adp.Ministerio==option_3)].groupby('Year_Convocatoria').agg({'Concursos':'sum'}).reset_index()
 
 with st.container:
     # grafico Convocatorias por Año
-    graf1=px.bar(publicaciones,x='Año',y='Year_Convocatoria',title='<b>Evolución de publicaciones ADP por año</b>',color_discrete_sequence=[color_bar]).\
+    graf1=px.bar(publicaciones,x='Year_Convocatoria',y='Concursos',title='<b>Evolución de publicaciones ADP por año</b>',color_discrete_sequence=[color_bar]).\
             update_yaxes(visible=visible_y_axis,title_text=None).\
                     update_xaxes(title_text=None,tickmode='linear', dtick=1,tickangle=-45)
     graf1.update_layout(yaxis_tickformat='.0f')
