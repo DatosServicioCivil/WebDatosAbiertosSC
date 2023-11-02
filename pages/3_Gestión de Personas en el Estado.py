89a58bafe3fc78f19bcf4c8d0497fe08e09d1b2a
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-02 17:55:07 -0300

    Update 3_Gestión de Personas en el Estado.py

--- pages/3_Gestión de Personas en el Estado.py
+++ pages/3_Gestión de Personas en el Estado.py
@@ -1,1 +1,53 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+# import snowflake.connector
+import pandas as pd
+from PIL import Image
+import plotly.express as px
+import numpy as np
+# import base64
+import time
+# import altair as alt
+import streamlit.components.v1 as components
+import plotly.express as px 
+import plotly.graph_objects as go
 
+st.set_page_config(layout='wide')
+
+# This function sets the logo and company name inside the sidebar
+def add_logo(logo_path, width, height):
+    """Read and return a resized logo"""
+    logo = Image.open(logo_path)
+    modified_logo = logo.resize((width, height))
+    return modified_logo
+
+my_logo = add_logo(logo_path="./imagenes/logo.png", width=200, height=100)
+st.image(my_logo)
+
+# Set Page Header
+st.header("Gestión de Personas en el Estado")
+# Set custom CSS for hr element
+st.markdown(
+    """
+        <style>
+            hr {
+                margin-top: 0.0rem;
+                margin-bottom: 0.5rem;
+                height: 3px;
+                background-color: #333;
+                border: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Add horizontal line
+st.markdown("<hr>", unsafe_allow_html=True)
+
+# define si se ven los ejes Y
+visible_y_axis=True
+    
+#https://framework.digital.gob.cl/colors.html
+color_line='#2D717C'
+color_bar='#6633CC'
