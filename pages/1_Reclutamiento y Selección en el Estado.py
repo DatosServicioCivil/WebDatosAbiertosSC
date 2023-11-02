f3ce3fc169d09dac87ad05dbb53b1925a8bb4099
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-02 16:49:03 -0300

    Update 1_Reclutamiento y Selección en el Estado.py
    se crea lista de ministerios
    se agrega valor todos al listado

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -52,16 +52,26 @@ Nivel = pd.concat([nuevo_registro, Nivel])
 Nivel = Nivel.reset_index(drop=True)
 Nivel = Nivel['Nivel'].tolist()
 
+unique_ministerios = df_concursos['Ministerio'].unique()
+Ministerios = pd.DataFrame({'Ministerio': unique_ministerios})
+nuevo_registro = pd.DataFrame({'Ministerio': ['Todos']})
+Ministerios = pd.concat([nuevo_registro, Ministerios])
+Ministerios = Ministerios.reset_index(drop=True)
+Ministerios = Ministerios['Ministerio'].tolist()
+Ministerios
 
-#Nivel=['','Nivel I', 'Nivel II']
-Ministerios=['','Ministerio de Hacienda', 'Ministerio de Educación','Ministerio de Economía, Fomento y Turismo', 'Ministerio de Salud',
-       'Ministerio del Trabajo y Previsión Social','Ministerio de Agricultura', 'Ministerio del Deporte',
-       'Ministerio de Minería', 'Ministerio de Energía','Ministerio de Defensa Nacional', 'Ministerio de Obras Públicas',
-       'Ministerio de Justicia y Derechos Humanos','Ministerio del Interior y Seguridad Pública',
-       'Ministerio de las Culturas, las artes y el Patrimonio','Ministerio de Desarrollo Social y Familia','Ministerio de Vivienda y Urbanismo',
-       'Ministerio de Ciencia, Tecnología, Conocimiento e Innovación','Ministerio de Relaciones Exteriores',
-       'Ministerio del Medio Ambiente','Ministerio de Transportes y Telecomunicaciones','Ministerio de la Mujer y la Equidad de Género',
-       'Ministerio Secretaría General de Gobierno', 'Autónomo','Administración Central',]
+
+
+
+# Ministerios=['','Ministerio de Hacienda', 'Ministerio de Educación','Ministerio de Economía, Fomento y Turismo', 'Ministerio de Salud',
+#       'Ministerio del Trabajo y Previsión Social','Ministerio de Agricultura', 'Ministerio del Deporte',
+#       'Ministerio de Minería', 'Ministerio de Energía','Ministerio de Defensa Nacional', 'Ministerio de Obras Públicas',
+#       'Ministerio de Justicia y Derechos Humanos','Ministerio del Interior y Seguridad Pública',
+#       'Ministerio de las Culturas, las artes y el Patrimonio','Ministerio de Desarrollo Social y Familia','Ministerio de Vivienda y Urbanismo',
+#       'Ministerio de Ciencia, Tecnología, Conocimiento e Innovación','Ministerio de Relaciones Exteriores',
+#       'Ministerio del Medio Ambiente','Ministerio de Transportes y Telecomunicaciones','Ministerio de la Mujer y la Equidad de Género',
+#       'Ministerio Secretaría General de Gobierno', 'Autónomo','Administración Central',]
+
 Region=['','Región de Metropolitana de Santiago','Región de Magallanes y de la Antártica Chilena',
        'Región del Libertador General Bernardo OHiggins','Región del Maule', 'Región del Biobío', 'Región de Los Ríos',
        'Región de  Valparaíso', 'Región de Los Lagos','Región de Arica y Parinacota', 'Región de la Araucanía',
