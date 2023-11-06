8f89131d35634a66c92829d10bd766c19d56b656
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-06 12:59:12 -0300

    Update 1_Reclutamiento y Selección en el Estado.py

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -434,10 +434,10 @@ if a=='Empleo Público':
     #----------------------------------------------------------------------------------------------------------------------------
     # grafico Convocatorias por Año
     graf4=px.bar(convocatorias,x='Year_Convocatoria',y='Convocatorias',title='<b>Evolución de convocatorias por año</b>',color_discrete_sequence=[color_bar]).\
-            update_yaxes(visible=visible_y_axis,title_text=None,type='linear', dtick=5000).\  # Se puede cambiar type de 'linear' a 'log' dtick es el intervalo
+            update_yaxes(visible=visible_y_axis,title_text=None,type='linear', dtick=5000).\  
                     update_xaxes(title_text=None,tickmode='linear', dtick=1,tickangle=-45)
     graf4.update_layout(yaxis_tickformat='.0f')
-
+    # Se puede cambiar type de 'linear' a 'log' dtick es el intervalo
 
     graf7=px.bar(convocatorias_x_tipo, x='Year_Convocatoria', y='idConcurso',
              color='Tipo postulacion',labels={'idConcurso': 'Cantidad de Convocatorias'}).\
