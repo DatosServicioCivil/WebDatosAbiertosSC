9668b0b7d146cb5636932aec6c26aa8343c34df0
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-06 17:04:33 -0300

    Update 1_Reclutamiento y Selección en el Estado.py

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -387,8 +387,7 @@ if a=='Empleo Público':
         
         vacantes_x_tipo['Porcentaje_2']=np.round(vacantes_x_tipo.Vacantes_x_tipo/vacantes_x_tipo.Vacantes,2)*100
 
-        rentas=df_concursos_eepp[df_concursos_eepp['Renta Bruta']!=0]
-        rentas=rentas[filtro]
+        rentas=df_concursos_eepp[filtro].query('Renta Bruta'!=0)
 
     convocatorias_vacantes=pd.merge(convocatorias_x_tipo,vacantes_x_tipo,how='left',on=['Year_Convocatoria','Tipo postulacion'])
     convocatorias_vacantes['Vacantes_x_Convocatoria']=np.round(convocatorias_vacantes.Vacantes_x_tipo/convocatorias_vacantes.Convocatorias_x_tipo,2)
