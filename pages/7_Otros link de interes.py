f380011da58eb181010bd08effde720e32723b54
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-10 08:14:12 -0300

    Update 7_Otros link de interes.py

--- pages/7_Otros link de interes.py
+++ pages/7_Otros link de interes.py
@@ -1,1 +1,41 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as components
+from PIL import Image
+import pandas as pd
+import numpy as np
+
+
+st.set_page_config(layout='wide')
+
+# This function sets the logo and company name inside the sidebar
+def add_logo(logo_path, width, height):
+    """Read and return a resized logo"""
+    logo = Image.open(logo_path)
+    modified_logo = logo.resize((width, height))
+    return modified_logo
+
+my_logo = add_logo(logo_path="./imagenes/logo.png", width=200, height=100)
+st.image(my_logo)
+
+# Set Page Header
+st.header("Otros links de interes")
+# Set custom CSS for hr element
+st.markdown(
+    """
+        <style>
+            hr {
+                margin-top: 0.0rem;
+                margin-bottom: 0.5rem;
+                height: 3px;
+                background-color: #333;
+                border: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Add horizontal line
+st.markdown("<hr>", unsafe_allow_html=True)
 
