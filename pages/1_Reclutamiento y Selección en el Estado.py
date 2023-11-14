fa6f76076b87e5f30933d82b64bd703ff8528a36
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-14 14:31:26 -0300

    Update 1_Reclutamiento y Selección en el Estado.py

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -216,7 +216,7 @@ if a=='Alta Dirección Pública':
     # Add annotation under the x-axis without arrowhead
     graf3.add_annotation(
     text="Se consideran solo concursos con nóminas enviadas",  # Annotation text
-    x=2004,  # X-coordinate where the annotation is placed
+    x=2010,  # X-coordinate where the annotation is placed
     y=0,             # Y-coordinate set to 0 to place the annotation under the x-axis
     showarrow=False  # Do not display an arrowhead
 )
