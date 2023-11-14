70aebd6e531e08e7b74b27f0eb8ed523a28d4358
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-14 14:29:41 -0300

    Update 1_Reclutamiento y Selección en el Estado.py

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -216,7 +216,7 @@ if a=='Alta Dirección Pública':
     # Add annotation under the x-axis without arrowhead
     graf3.add_annotation(
     text="Note under X-axis",  # Annotation text
-    x=1,  # X-coordinate where the annotation is placed
+    x=2004,  # X-coordinate where the annotation is placed
     y=0,             # Y-coordinate set to 0 to place the annotation under the x-axis
     showarrow=False  # Do not display an arrowhead
 )
