dc7756e42fe050d837eb437e66193f1a2d29694e
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-17 15:45:59 -0300

    correccion graf4

--- pages/3_Gestión de Personas en el Estado.py
+++ pages/3_Gestión de Personas en el Estado.py
@@ -244,9 +244,9 @@ if a=='Capacitación en el Estado':
     graf4 = go.Figure()
     graf4.add_trace(go.Treemap(
     ids=df_treemap.Ministerio,
-    labels=df_treemap.Servicio,
-    parents=df_treemap.Todos,  # Usar 'Todos' como el nodo raíz
-    maxdepth=3,
+    labels=df_treemap.Ministerio,   
+    parents=df_treemap.Ministerio,  # Usar 'Todos' como el nodo raíz
+    maxdepth=-1,
     root_color="lightgrey",
     values=df_treemap.Inversion))
 
