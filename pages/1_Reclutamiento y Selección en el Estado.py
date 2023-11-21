a84729568810bc40f0b4a3500acfad62be0f3086
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-21 09:34:30 -0300

    modificacion graf3 a concursos desiertos/anulados

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -694,7 +694,7 @@ if a=='Directores para Chile':
     #        update_yaxes(visible=visible_y_axis,title_text=None).\
     #                update_xaxes(title_text=None,tickmode='linear', dtick=1)
     # , color="Estado_Concurso"
-    graf3=px.bar(df_estados, x="Año", y="Convocatorias", title="Estados").\
+    graf3=px.bar(df_estados[df_estados['Estado_Concurso'].isin('Desierto','Anulado')], x="Año", y="Convocatorias",color='Estado_Concurso' ,title="Concursos Desiertos o Anulados").\
              update_yaxes(visible=visible_y_axis,title_text=None).\
                     update_xaxes(title_text=None,tickmode='linear', dtick=1).\
                         update_layout(legend=dict(x=0.5, xanchor='center', y=-0.1, yanchor='top', traceorder='normal', itemsizing='trace',orientation='h'))  # Ubicar debajo del eje x en dos columnas
