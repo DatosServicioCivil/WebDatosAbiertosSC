8a6ed2c68228f3e11798f7f40f1980af9d45cdab
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-27 14:13:50 -0300

    mejora selcol5

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -336,7 +336,7 @@ if a=='Alta Dirección Pública':
         with col4:
             st.plotly_chart(graf5,use_container_width=True)
         with col5:
-            sel_col5=st.radio('Seleccione',('Gráfico','Tabla'))
+            sel_col5=st.radio('Seleccione',('Gráfico','Tabla'),horizontal=True)
             if sel_col5=='Gráfico':
                 st.plotly_chart(graf7,use_container_width=True)
             else:
