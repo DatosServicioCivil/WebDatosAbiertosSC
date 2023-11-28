79714c3822b8b6c6ef7fb367ea13f961eb5b5085
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-28 17:37:41 -0300

    cambio indexacion st.dataframe(df_desiertos)

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -689,7 +689,7 @@ if a=='Empleo Público':
             st.plotly_chart(graf11,use_container_width=True)
 
     #st.text(df_concursos_eepp.Estado.unique())
-    st.dataframe(df_desiertos)
+st.dataframe(df_desiertos)
 
 #----------------------------------------------------------------------------------------------------------------------
 
