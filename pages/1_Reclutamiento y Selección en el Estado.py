f00ff273a7d0f664c5add1ac1c03a904256fb4db
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-28 17:14:01 -0300

    st.text estado.unique

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -676,7 +676,7 @@ if a=='Empleo Público':
     with col6:
             st.plotly_chart(graf11,use_container_width=True)
 
-df_concursos_eepp.Estado.unique()
+st.text(df_concursos_eepp.Estado.unique())
 st.dataframe(df_concursos_eepp)
 
 #----------------------------------------------------------------------------------------------------------------------
