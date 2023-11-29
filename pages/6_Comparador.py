95da24b836505300ef5f543458bb6d8da8f7fcbd
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-11-29 15:46:12 -0300

    pagina comparador

---
+++ pages/6_Comparador.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+# import snowflake.connector
+import pandas as pd
+from PIL import Image
+import plotly.express as px
+import numpy as np
+# import base64
+import time
+# import altair as alt
+import streamlit.components.v1 as components
+import plotly.express as px 
+import plotly.graph_objects as go
+
+st.set_page_config(layout='wide')
+
+# This function sets the logo and company name inside the sidebar
+def add_logo(logo_path, width, height):
+    """Read and return a resized logo"""
+    logo = Image.open(logo_path)
+    modified_logo = logo.resize((width, height))
+    return modified_logo
+
+my_logo = add_logo(logo_path="./imagenes/logo.png", width=200, height=100)
+st.image(my_logo)
+
+# Set Page Header
+st.header("Comparador de datos")
+# Set custom CSS for hr element
+st.markdown(
+    """
+        <style>
+            hr {
+                margin-top: 0.0rem;
+                margin-bottom: 0.5rem;
+                height: 3px;
+                background-color: #333;
+                border: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Add horizontal line
+st.markdown("<hr>", unsafe_allow_html=True)
+
+# define si se ven los ejes Y
+visible_y_axis=True
