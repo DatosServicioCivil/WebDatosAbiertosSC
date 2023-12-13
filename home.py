9161144fd047cd4e6bba395340bf7e0add6793ec
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-12-13 16:21:02 -0300

    cambio formato en imagenes y csv a parquet

--- home.py
+++ home.py
@@ -9,6 +9,7 @@ import numpy as np
 import time
 # import altair as alt
 import streamlit.components.v1 as components
+import pyarrow.parquet as pq
 
 st.set_page_config(layout='wide',
                    initial_sidebar_state="expanded")                                #collapsed
@@ -67,11 +68,24 @@ st.markdown(
 st.markdown("<hr>", unsafe_allow_html=True)
 
 #--------------------------------------------------------------------------------------------
-df_concursos_eepp_aviso=pd.read_csv('EEPP/df_concursos_eepp_Aviso.csv',sep=";",encoding='utf-8')
-df_concursos_eepp_Postulacion=pd.read_csv('EEPP/df_concursos_eepp_Postulacion en linea.csv',sep=";",encoding='utf-8')
-df_concursos_eepp=pd.concat([df_concursos_eepp_aviso,df_concursos_eepp_Postulacion])
+@st.cache_data
+def df_conc_ep():
+    df_concursos_eepp_Postulacion=pq.read_table('EEPP/df_concursos_eepp_Postulacion en linea.parquet').to_pandas()
+    df_concursos_eepp_aviso=pd.read_table('EEPP/df_concursos_eepp_Aviso.parquet').to_pandas()
+    df_concursos_eepp=pd.concat([df_concursos_eepp_aviso,df_concursos_eepp_Postulacion])
+    df_conc_ep=df_concursos_eepp
+    return df_conc_ep
 
-df_concursos_adp=pd.read_csv('ADP/df_concursos.csv',sep=";",encoding='utf-8')
+
+@st.cache_data
+def df_con_adp():
+    df_concursos_adp=pq.read_table('ADP/df_concursos.parquet').to_pandas()
+    df_conc_adp=df_concursos_adp
+    return df_conc_adp
+
+
+df_concursos_eepp=df_conc_ep()
+df_concursos_adp=df_con_adp()
 
 
 vacantes = df_concursos_eepp.agg({'Nº de Vacantes':'sum'}).reset_index()
@@ -81,41 +95,46 @@ concursos_adp=df_concursos_adp.CD_Concurso.count()
 nombrados_adp=df_concursos_adp.query("Estado=='Nombrado'").CD_Concurso.count()
 
 
-
 with st.container():
     col1,col2,col3,col4,col5,col6=st.columns(6,gap='small')
     with col1:
-        image = Image.open('imagenes/job_application.jpg')
+        #image = Image.open('imagenes/job_application.jpg')
+        image=add_logo(logo_path="./imagenes/job_application.jpg", width=150, height=150)
         st.image(image)
         valor_col1=f"{postulaciones:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col1}</h1>", unsafe_allow_html=True)
         st.markdown("<h2 style='text-align: center; color: grey;'>Total postulaciones portal EEEPP</h2>", unsafe_allow_html=True)
     with col2:
-        image = Image.open('imagenes/job_offer.png')
+        #image = Image.open('imagenes/job_offer.png')
+        image=add_logo(logo_path="./imagenes/job_offer.png", width=150, height=150)
         st.image(image)
         valor_col2=f"{vacantes.iat[0,1]:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col2}</h1>", unsafe_allow_html=True)
         st.markdown("<h2 style='text-align: center; color: grey;'>Total vacantes ofrecidas EEPP</h2>", unsafe_allow_html=True)
     with col3:
-        image = Image.open('imagenes/mannager_selection.png')
+        #image = Image.open('imagenes/mannager_selection.png')
+        image=add_logo(logo_path="./imagenes/mannager_selection.png", width=150, height=150)
         st.image(image)
         valor_col3=f"{concursos_adp:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col3}</h1>", unsafe_allow_html=True)
         st.markdown("<h2 style='text-align: center; color: grey;'>Total concursos ADP</h2>", unsafe_allow_html=True)
     with col4:
-        image = Image.open('imagenes/adp_nombrado.PNG')
+        #image = Image.open('imagenes/adp_nombrado.PNG')
+        image=add_logo(logo_path="./imagenes/adp_nombrado.PNG", width=150, height=150)
         st.image(image)
         valor_col4=f"{nombrados_adp:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col4}</h1>", unsafe_allow_html=True)
         st.markdown("<h2 style='text-align: center; color: grey;'>Total nombramientos ADP</h2>", unsafe_allow_html=True)
     with col5:
-        image = Image.open('imagenes/Directores.png')
+        #image = Image.open('imagenes/Directores.png')
+        image=add_logo(logo_path="./imagenes/Directores.png", width=150, height=150)
         st.image(image)
         valor_col5=f"{nombrados_adp:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col5}</h1>", unsafe_allow_html=True)
         st.markdown("<h2 style='text-align: center; color: grey;'>Personas seleccionadas en Prácticas Chile</h2>", unsafe_allow_html=True)
     with col6:
-        image = Image.open('imagenes/trainee.PNG')
+        #image = Image.open('imagenes/trainee.PNG')
+        image=add_logo(logo_path="./imagenes/trainee.PNG", width=150, height=150)
         st.image(image)
         valor_col6=f"{nombrados_adp:,}".replace(",", ".")
         st.markdown(f"<h1 style='text-align: center; color: grey;'>{valor_col6}</h1>", unsafe_allow_html=True)
