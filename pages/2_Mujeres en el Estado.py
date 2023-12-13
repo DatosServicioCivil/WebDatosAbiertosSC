0bd7824e94b4d7d01106cefbf361fa2166cdb427
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-12-13 09:38:56 -0300

    correccion df_tabla_deem

--- pages/2_Mujeres en el Estado.py
+++ pages/2_Mujeres en el Estado.py
@@ -38,8 +38,8 @@ def df_post_deem():
 @st.cache_data
 def df_tabla_deem():
     df=pq.read_table('DEEM/df_concursos_dee.parquet').to_pandas()
-    df_tabla_deem=df
-    return df_tabla_deem
+    df_tab_deem=df
+    return df_tab_deem
 
 # se asocia concursos, cargos, nivel a postulaciones
 df_concursos=pq.read_table('ADP/df_concursos.parquet').to_pandas()
@@ -61,8 +61,8 @@ df_concursos_eepp['Año']=pd.to_datetime(df_concursos_eepp['Fecha Final Proceso'
 Porcentaje_Mujeres_Seleccionadas_Jefaturas_EEPP=df_concursos_eepp[(df_concursos_eepp['Tipo Base']=='Jefe Departamento')]['SeleccionadoMujeres'].sum()\
     /df_concursos_eepp[(df_concursos_eepp['Tipo Base']=='Jefe Departamento')]['Seleccionados'].sum()
 
-Porcentaje_Mujeres_Nombradas_DEEM=df_tabla_deem[(df_tabla_deem['Estado']=='Nombrado')\
-                                                & (df_tabla_deem['SexoNombrado']!='Femenino')]['idConcurso'].count()\
+df_tabla_deem=df_tabla_deem()
+Porcentaje_Mujeres_Nombradas_DEEM=df_tabla_deem[(df_tabla_deem['Estado']=='Nombrado') & (df_tabla_deem['SexoNombrado']!='Femenino')]['idConcurso'].count()\
                                                 /df_tabla_deem[(df_tabla_deem['Estado']=='Nombrado')\
                                                 & (df_tabla_deem['SexoNombrado']!='Sin Inform Portal-GeeDem')\
                                                 & (df_tabla_deem['SexoNombrado']!='')]['idConcurso'].count()
