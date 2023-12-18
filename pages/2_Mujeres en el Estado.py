e8c5f68b6df9f72e0e9ef89823114561c59477c8
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-12-18 08:19:54 -0300

    metodo de detectar el nombre en tb_porcentajes_sexo_año

--- pages/2_Mujeres en el Estado.py
+++ pages/2_Mujeres en el Estado.py
@@ -181,16 +181,19 @@ with st.container():
 if option_1=='Todos':
     tb_postulaciones_sexo_año = tb_postulaciones.groupby(['Año', 'Sexo'])['postulaciones'].sum().reset_index()
     tb_porcentajes_sexo_año=tb_postulaciones_sexo_año.groupby(['Año'])['postulaciones'].apply(lambda x: 100 * x / x.sum()).reset_index()
-    tb_porcentajes_sexo_año=tb_porcentajes_sexo_año.rename(columns={'Porcentaje':'postulaciones'})
+    #tb_porcentajes_sexo_año=tb_porcentajes_sexo_año.rename(columns={'Porcentaje':'postulaciones'})
 else:
     tb_postulaciones_sexo_año = tb_postulaciones[tb_postulaciones['portal']==option_1].groupby(['Año', 'Sexo'])['postulaciones'].sum().reset_index()
     tb_porcentajes_sexo_año=tb_postulaciones_sexo_año[tb_postulaciones['portal']==option_1].groupby(['Año'])['postulaciones'].apply(lambda x: 100 * x / x.sum()).reset_index()
-    tb_porcentajes_sexo_año=tb_porcentajes_sexo_año.rename(columns={'Porcentaje':'postulaciones'})
+    #tb_porcentajes_sexo_año=tb_porcentajes_sexo_año.rename(columns={'Porcentaje':'postulaciones'})
 
 
 
 # cambio de nombre de columnas
 tb_3=pd.concat([tb_postulaciones_sexo_año,tb_porcentajes_sexo_año],axis=1)
+posicion_columna = 3
+nombre_actual = tb_porcentajes_sexo_año.columns[posicion_columna]
+tb_porcentajes_sexo_año.rename(columns={nombre_actual: 'Porcentaje'}, inplace=True)
 #tb_porcentajes_sexo_año=pd.concat([tb_postulaciones_sexo_año,tb_porcentajes_sexo_año],axis=1)
 #-------------------------------------------------------------------------------------------------------------
 #gráfico postulaciones por año y sexo segun seleccion portal
