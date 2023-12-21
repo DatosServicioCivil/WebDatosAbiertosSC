fb51b695cb5784d6440645cd810f8f25b70c72e7
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2023-12-21 17:36:12 -0300

    pd.merge(tb_nombramiento_sexo_ministerio,tb_nombramiento_adp_ministerio

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -469,6 +469,7 @@ if a=='Alta Dirección Pública':
         tb_nombramiento_adp_ministerio=tb_nombramiento_adp_ministerio.rename(columns={'postulaciones': 'Total Nombramientos'})    
         tb_nombramiento_sexo=pd.merge(tb_nombramiento_sexo,tb_nombramiento_adp,how='left',on='Año')
         tb_nombramiento_sexo['Porcentaje']=tb_nombramiento_sexo['postulaciones']/tb_nombramiento_sexo['Nombramientos']
+        tb_nombramiento_sexo_ministerio=pd.merge(tb_nombramiento_sexo_ministerio,tb_nombramiento_adp_ministerio,how='left',on='Ministerio')
 
 
 
