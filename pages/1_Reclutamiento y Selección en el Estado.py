2af79e5e6a508d24c19848ae8c6bd05ca9ffbaaf
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-02 14:06:37 -0300

    filtro 5 6 y 7

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -425,6 +425,14 @@ if a=='Alta Dirección Pública':
             postulaciones=df_postulaciones_adp[(df_postulaciones_adp['Ministerio']==option_3)].groupby('Año').agg({'postulaciones':'sum'}).reset_index()
             postulaciones_x_ministerio=df_postulaciones_adp[(df_postulaciones_adp['Ministerio']==option_3)].groupby('Ministerio').agg({'postulaciones':'sum'}).reset_index()
 
+        if option_1=='Todos' and option_2=='Todos' and option_3!='Todos' and option_4=='Todos' and option_5!='Todos': #6
+            postulaciones=df_postulaciones_adp[(df_postulaciones_adp['Ministerio']==option_3) & (df_postulaciones_adp['Sexo']==option_5)].groupby('Año').agg({'postulaciones':'sum'}).reset_index()
+            postulaciones_x_ministerio=df_postulaciones_adp[(df_postulaciones_adp['Ministerio']==option_3) & (df_postulaciones_adp['Sexo']==option_5)].groupby('Ministerio').agg({'postulaciones':'sum'}).reset_index()
+
+        if option_1!='Todos' and option_2=='Todos' and option_3!='Todos' and option_4=='Todos' and option_5!='Todos': #7
+            postulaciones=df_postulaciones_adp[(df_postulaciones_adp['Nivel']==option_1) & (df_postulaciones_adp['Ministerio']==option_3) & (df_postulaciones_adp['Sexo']==option_5)].groupby('Año').agg({'postulaciones':'sum'}).reset_index()
+            postulaciones_x_ministerio=df_postulaciones_adp[(df_postulaciones_adp['Nivel']==option_1) & (df_postulaciones_adp['Ministerio']==option_3) & (df_postulaciones_adp['Sexo']==option_5)].groupby('Ministerio').agg({'postulaciones':'sum'}).reset_index()
+
         st.dataframe(postulaciones.head(10))
         st.dataframe(postulaciones_x_ministerio.head(10))
         # ----------------------------------------------------------------------------------------------------------------
