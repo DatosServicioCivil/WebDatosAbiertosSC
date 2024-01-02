5094f4ab8e742e1d3275fa387c37a948a6bbd69e
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-02 08:07:01 -0300

    texto_foot_1

--- home.py
+++ home.py
@@ -141,3 +141,6 @@ with st.container():
 
 #image = Image.open('./imagenes/datosabiertos.png')
 #st.image(image, width=1000)
+with st.container():
+    texto_foot_1="""<strong>¿Qué son los Datos Abiertos?</strong>"""
+    st.caption(texto_foot_1, unsafe_allow_html=False, *, help=None)
