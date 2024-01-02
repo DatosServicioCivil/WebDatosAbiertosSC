210d2c5f6ff021d99f4f39224a8b3c19930d887d
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-02 17:30:35 -0300

    prueba

--- pages/3_Gestión de Personas en el Estado.py
+++ pages/3_Gestión de Personas en el Estado.py
@@ -646,13 +646,13 @@ if a=='Normas de Gestión de Personas':
         with col1:
             col_1_1,col_1_2,col_1_3,col_1_4,col_1_5=st.columns(5,gap='small')
             texto_Norma_1="""Porcentaje de Jefaturas de Gestión de Personas que se encuentran en un 2do o 3er Nivel Jerárquico"""
-            valor_col_1_1=Norma_1[Norma_1['Año']==2018].Metrica
+            valor_col_1_1=f"{Norma_1.query('Año==2018').Metrica}"
             valor_col_1_2=Norma_1[Norma_1['Año']==2019].Metrica
             valor_col_1_3=Norma_1[Norma_1['Año']==2020].Metrica
             valor_col_1_4=Norma_1[Norma_1['Año']==2021].Metrica
             valor_col_1_5=Norma_1[Norma_1['Año']==2022].Metrica
             with col_1_1:
-                st.markdown("<h3 style='text-align: center; color: grey;'>{valor_col_1_1}</h3>", unsafe_allow_html=True)
+                st.markdown("<h3 style='text-align: center; color: grey;'>valor_col_1_1</h3>", unsafe_allow_html=True)
                 st.markdown("<h3 style='text-align: center; color: grey;'>2018</h3>", unsafe_allow_html=True)
             with col_1_2:
                 st.markdown("<h3 style='text-align: center; color: grey;'>{valor_col_1_2}</h3>", unsafe_allow_html=True)
