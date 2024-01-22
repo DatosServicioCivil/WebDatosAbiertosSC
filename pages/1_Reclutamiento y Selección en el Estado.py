39fe30b74ccd9f32b54ac9d66e445eb0ef9a20b2
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-22 08:17:58 -0300

    df_sel_eepp

--- pages/1_Reclutamiento y Selección en el Estado.py
+++ pages/1_Reclutamiento y Selección en el Estado.py
@@ -1082,7 +1082,8 @@ if a=='Empleo Público':
         def seleccionados_eepp():
             df_sel_eepp=pq.read_table('EEPP/df_seleccionados_eepp.parquet').to_pandas()
             df_sel_eepp=pd.merge(df_sel_eepp,all_region,left_on='region_postulante',right_on='Region',how='left')
-            df_sel_eepp=df_sel_eepp.drop(columns=['Region','region_postulante'])
+            df_sel_eepp=df_sel_eepp.drop(columns=['Region','region_postulante','ID_Region'])
+            df_sel_eepp=df_sel_eepp.rename(columns={'Region':'region_homologada_postulante'})
             return df_sel_eepp
 
         df_seleccionados_eepp=seleccionados_eepp()
